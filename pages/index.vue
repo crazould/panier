8ab470f4de7be5eebe659e23e4b195fa74584287
crazould <53102589+crazould@users.noreply.github.com>
2022-10-20 13:15:28 +0700

    add global style

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,33 @@
 <template>
-<h1>
-  panier
-</h1>
+  <div>
+    <header>
+      <h1>Panier - Shopping Cart App</h1>
+    </header>
+    <main>
+
+    </main>
+    <footer>© {{ year }} Thariq, All right reserved</footer>
+  </div>
 </template>
 
+<style scoped>
+  main{
+    min-height: 90vh;
+  }
+</style>
+
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  computed: {
+    year() {
+      return new Date().getFullYear();
+    },
+  },
+  data() {
+    return {};
+  },
+});
 </script>
